463d8abc2792fb8d35d382c8d57cee178517f605
DimaZh <DimaZh@DZ-Laptop-Dell>
2015-07-16 17:17:29 +0400

    My project

---
+++ Java_ODCS_test/src/java_odcs_test/Java_ODCS_test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_odcs_test;
+
+/**
+ *
+ * @author DimaZh
+ */
+public class Java_ODCS_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi Java support in Tailwind!");
+        // TODO code application logic here
+    }
+    
+}
